3f27cd2eaedb46fc014eac5d011ce6902166589a
Jacko <jacko.tinoco@tecsup.edu.pe>
2022-10-31 15:26:55 -0500

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=0;$i<50;$i++){
+            User::create(
+                [
+                    'nombre'=>"usuario$i",
+                    'email'=>"email$i@test.com",
+                    'password'=>bcrypt("pass$i"),
+                ]
+            );
+        }
+    }
+}
